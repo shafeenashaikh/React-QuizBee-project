bd51e8e1ebeffd5c955af81b783640b6805a2ef1
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-27 10:26:54 +0530

    bringing set of question from offline API(quizService) using componentDidMount method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
 import './assets/style.css'
+import quizService from './quizService'
 
 class QuizBee extends Component{
+
+    state = {
+        questionBank: []
+    };
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    componentDidMount(){
+        this.getQuestions();
+    }
     render(){
         return(
             <div className="container">
                 <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 &&
+                    this.state.questionBank.map(
+                        ({question, answer, correct, questionId}) => <h4>{question}</h4>
+                    )
+
+                }
+
             </div>
         )
     }
